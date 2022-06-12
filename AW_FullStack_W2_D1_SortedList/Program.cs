409843d29c6b6229c6f302d6bbc8c735e5c66d85
Martti Tuisku <martti.tuisku@sherpas.se>
2022-06-12 20:28:29 +0200

    Adding SortedList testbase

---
+++ AW_FullStack_W2_D1_SortedList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AW_FullStack_W2_D1_SortedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<int, string> yksinkertainen = new SortedList<int, string>();
+
+            yksinkertainen.Add(1, "Red Bull");
+            yksinkertainen.Add(2, "Ferrari");
+            yksinkertainen.Add(3, "Mercedes AMG");
+            yksinkertainen.Add(4, "McLaren");
+            yksinkertainen.Add(5, "Alfa Romeo");
+            yksinkertainen.Add(6, "Alpine");
+            yksinkertainen.Add(7, "Alfa Tauri");
+            yksinkertainen.Add(8, "Aston Martin");
+            yksinkertainen.Add(9, "Haas");
+            yksinkertainen.Add(10, "Williams");
+
+            foreach (KeyValuePair<int, string> pair in yksinkertainen)
+            {
+                Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+            }
+
+            Console.WriteLine(yksinkertainen.Capacity);
+
+            Console.WriteLine(yksinkertainen.Count);
+
+        }
+    }
+}
